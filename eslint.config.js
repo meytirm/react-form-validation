9e53460fbf6efb4c64cd1c8c53b87ffe1c250cc8
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-16 12:43:57 +0330

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx,js}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'array-bracket-spacing': ['error', 'never'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'comma-dangle': ['error', 'always-multiline'],
+      'indent': ['error', 2],
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'object-curly-spacing': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'space-before-function-paren': ['error', 'never'],
+    },
+  },
+]);
