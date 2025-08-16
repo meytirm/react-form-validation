1faa41cba2061eece4e5682534ec14255d9b8446
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-16 16:03:02 +0330

    feat(app): integrate FormProvider and FormInput into App
    Integrate FormProvider and FormInput components into the App component. Added form state management, input handling, and a basic submit function for improved functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import FormProvider from './store/FormProvider.tsx';
+import FormInput from './components/FormInput.tsx';
+import { type FormEvent, useState } from 'react';
+
 function App() {
+  const [value, setValue] = useState('');
+  const [value2, setValue2] = useState('ss');
+  function handleOnSubmit(event: FormEvent) {
+    console.log(event);
+  }
 
   return (
-    <div>hi</div>
+    <FormProvider onSubmit={handleOnSubmit}>
+      <FormInput value={value} onChange={setValue} rules={[(val: string) => !!val || 'error']}/>
+      <FormInput value={value2} onChange={setValue2} rules={[(val: string) => !!val || 'empty']}/>
+    </FormProvider>
   );
 }
 
