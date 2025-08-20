408ea0870866883d1949ebd25113a57575fcca83
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-21 00:06:06 +0330

    feat(types): add InputHTMLProps definition
    Introduce InputHTMLProps type to extend InputHTMLAttributes, omitting `onChange` and `value`. Enhances type support for custom input components using React.

--- src/types/input.ts
+++ src/types/input.ts
@@ -1,6 +1,10 @@
+import type { InputHTMLAttributes } from 'react';
+
 export type InputInstanceType = {
   validate: () => boolean;
   reset: () => void;
 }
 
 export type RuleType = (value: string) => boolean | string
+
+export type InputHTMLProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'value'>
