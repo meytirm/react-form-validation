ba42c61544290233d734c6a12337ea019c732d92
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-22 22:49:24 +0330

    feat(store): add imperative handle to FormProvider for form methods
    Enhance `FormProvider` to use `useImperativeHandle`, exposing `validate` and `reset` methods via ref. Updates Props type to include an optional `ref`. Removes unnecessary `console.log` call within unregister logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import FormProvider from './store/FormProvider.tsx';
 import FormInput from './components/FormInput.tsx';
-import { type FormEvent, useState } from 'react';
+import { type FormEvent, useEffect, useRef, useState } from 'react';
 import { required } from './utils/rules.ts';
+import type { FromRefType } from './types/input.ts';
 
 function App() {
   const [value, setValue] = useState('');
   const [value2, setValue2] = useState('ss');
+  const formRef = useRef<null | FromRefType>(null);
   function handleOnSubmit(event: FormEvent) {
     console.log(event);
   }
 
+
+  useEffect(() => {
+    if (formRef.current) {
+      formRef.current.reset();
+      console.log(formRef.current.validate());
+    }
+  }, []);
+
   return (
-    <FormProvider onSubmit={handleOnSubmit}>
+    <FormProvider ref={formRef} onSubmit={handleOnSubmit}>
       <FormInput value={value} onChange={setValue} rules={[required()]}/>
       <FormInput value={value2} onChange={setValue2} rules={[required()]}/>
     </FormProvider>
