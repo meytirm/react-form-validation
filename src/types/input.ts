bc7ee7dfa0d416d1468248125fd0cc4c51f348f6
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-23 11:56:11 +0330

    refactor(types): generalize InputProps type
    Replace `InputHTMLProps` with a more generic `InputProps` type, allowing flexibility for various input element types.

--- src/types/input.ts
+++ src/types/input.ts
@@ -7,6 +7,6 @@ export type InputInstanceType = {
 
 export type RuleType = (value: string) => boolean | string
 
-export type InputHTMLProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'value'>
+export type InputProps<T> = Omit<InputHTMLAttributes<T>, 'onChange' | 'value'>
 
 export type FromRefType = { validate: () => boolean; reset: () => void; }
