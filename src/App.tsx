0764335d5805bab97018694d5b973ab885e39582
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-23 13:06:31 +0330

    feat(App): integrate FormTextarea component into App
    Add `FormTextarea` to `App` within `FormProvider`, enabling textarea input support with validation. Updates state binding and rules application for consistent form handling.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import FormInput from './components/FormInput.tsx';
 import { type FormEvent, useEffect, useRef, useState } from 'react';
 import { required } from './utils/rules.ts';
 import type { FromRefType } from './types/input.ts';
+import FormTextarea from './components/FormTextarea.tsx';
 
 function App() {
   const [value, setValue] = useState('');
   const [value2, setValue2] = useState('ss');
   const formRef = useRef<null | FromRefType>(null);
+
   function handleOnSubmit(event: FormEvent) {
     console.log(event);
   }
@@ -24,6 +26,7 @@ function App() {
     <FormProvider ref={formRef} onSubmit={handleOnSubmit}>
       <FormInput value={value} onChange={setValue} rules={[required()]}/>
       <FormInput value={value2} onChange={setValue2} rules={[required()]}/>
+      <FormTextarea onChange={setValue} value={value} rules={[required()]}></FormTextarea>
     </FormProvider>
   );
 }
